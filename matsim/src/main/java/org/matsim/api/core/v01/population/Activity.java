aa7f55f9403706763fba47a486e48fa8cd913f58
Thibaut Dubernet <thibautd@ethz.ch>
2018-08-30 11:16:12 +0200

    clean imports

--- matsim/src/main/java/org/matsim/api/core/v01/population/Activity.java
+++ matsim/src/main/java/org/matsim/api/core/v01/population/Activity.java
@@ -20,8 +20,6 @@
 
 package org.matsim.api.core.v01.population;
 
-import org.matsim.api.core.v01.BasicAddress;
-import org.matsim.api.core.v01.BasicLocation;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
